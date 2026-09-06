dec80f0ad0529e48ea981665cf040500555887aa
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-09-06 23:04:56 +0530

    Time: 3 ms (80.09%), Space: 202.6 MB (92.97%) - LeetHub

--- 4045-count-robot-groups/4045-count-robot-groups.cpp
+++ 4045-count-robot-groups/4045-count-robot-groups.cpp
@@ -3,14 +3,14 @@ public:
     int countGroups(vector<int>& position, vector<int>& speed, int distance) {
        for(int i=0;i<speed.size()-1;i++){
         if(position[i+1]-position[i]<=distance){
-            speed.erase(speed.begin()+i);
-            position.erase(position.begin()+i);
-            i--;
+            speed[i] = -1;
+            position[i] = -1;
         }
        }
        int mini = INT_MAX;
        int cnt = 1;
        for(int i=speed.size()-1;i>=0;i--){
+        if(speed[i]==-1) continue;
         if(mini == INT_MAX) mini = speed[i];
         else if(mini<speed[i]){
             continue;
